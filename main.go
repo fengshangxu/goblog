d11faddc0c25269e18592624538b821be1d1b96b
fengshangxu <fengshangxu@moyi365.com>
2021-03-11 15:25:29 +0800

    登录页面展示

--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import (
 )
 
 func main() {
+	funcConf()
 	beego.Run()
+}
+
+func funcConf() {
+	beego.AddFuncMap("appName", func() string { return "123" })
 }
--- routers/router.go
+++ routers/router.go
@@ -3,8 +3,10 @@ package routers
 import (
 	"github.com/astaxie/beego"
 	"goblog/controllers"
+	"goblog/controllers/admin"
 )
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
+	beego.Router("/admin/index", &admin.LoginController{}, "GET:Index")
 }
